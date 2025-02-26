09787523a89c2c0992c08417ab8abc2fcc19f3e3
ALEEF02 <thesmarty2002@gmail.com>
2025-02-25 20:57:47 -0500

    Replace MPRLS implementation in main code

--- pi/main.py
+++ pi/main.py
@@ -162,67 +162,7 @@ class Tank:
     def close(self):
         self.valve.close()
 
-
-class WrapMPRLS:
-    """
-    Wrap the MPRLS library to prevent misreads.
-    
-    multiplexerLine: The multiplexed i2c line. If not specified, this object will become dormant
-    """
-    
-    def __init__(self, multiplexerLine=False):
-        self.cantConnect = False
-        self.mprls = False
-        
-        if not multiplexerLine: # No multiplexer defined, therefore this is a blank object
-            self.cantConnect = True
-            return
-        
-        try:
-            self.mprls = adafruit_mprls.MPRLS(multiplexerLine, psi_min=0, psi_max=25)
-        except:
-            self.cantConnect = True
-        
-    def _get_pressure(self):
-        if self.cantConnect: return -1
-        return self.mprls.pressure
-
-    def _get_triple_pressure(self):
-        if self.cantConnect: return -1
-        pressures = []
-        for e in range(3):
-            pressures.append(self.mprls.pressure)
-            if e < 2: time.sleep(0.005) # MPRLS sample rate is 200 Hz https://forums.adafruit.com/viewtopic.php?p=733797
-            
-        return median(pressures)
-    
-    def _set_pressure(self, value):
-        pass
-
-    def _del_pressure(self):
-        pass
-        
-    """
-        Acts as a wrapper for the pressure property of the standard MPRLS
-    """
-    pressure = property(
-        fget=_get_pressure,
-        fset=_set_pressure,
-        fdel=_del_pressure,
-        doc="The pressure of the MPRLS or -1 if we can't connect to it"
-    )
-    
-    """
-        Adds ~10 ms of delay!
-        Acts as a wrapper for the pressure property of the standard MPRLS.
-    """
-    triple_pressure = property(
-        fget=_get_triple_pressure,
-        fset=_set_pressure,
-        fdel=_del_pressure,
-        doc="The 3-sample median pressure of the MPRLS or -1 if we can't connect to it"
-    )
-
+from pi.MPRLS import MPRLSWrappedSensor
 
 class PressuresOBJ:
     """Gather pressure information nicely."""
@@ -299,36 +239,36 @@ except:
     mprint.p("COULD NOT CONNECT TO MULTIPLEXER!! Time: " + str(timeMS()) + " ms", output_log)
 
 # Create blank objects
-mprls_canister = WrapMPRLS()
-mprls_bleed = WrapMPRLS()
-mprls_tank_1 = WrapMPRLS()
-mprls_tank_2 = WrapMPRLS()
-mprls_tank_3 = WrapMPRLS()
+mprls_canister = MPRLSWrappedSensor()
+mprls_bleed = MPRLSWrappedSensor()
+mprls_tank_1 = MPRLSWrappedSensor()
+mprls_tank_2 = MPRLSWrappedSensor()
+mprls_tank_3 = MPRLSWrappedSensor()
 
 if multiplex != False:
     # Canister MPRLS
-    mprls_canister = WrapMPRLS(multiplexerLine=multiplex[0])
-    if mprls_canister.cantConnect:
+    mprls_canister = MPRLSWrappedSensor(multiplexer_line=multiplex[0])
+    if mprls_canister.cant_connect:
         mprint.p("COULD NOT CONNECT TO CANISTER MPRLS!! Time: " + str(timeMS()) + " ms", output_log)
 
     # Bleed Tank MPRLS
-    mprls_bleed = WrapMPRLS(multiplexerLine=multiplex[1])
-    if mprls_bleed.cantConnect:
+    mprls_bleed = MPRLSWrappedSensor(multiplexer_line=multiplex[1])
+    if mprls_bleed.cant_connect:
         mprint.p("COULD NOT CONNECT TO BLEED MPRLS!! Time: " + str(timeMS()) + " ms", output_log)
 
     # Tank 1 MPRLS
-    mprls_tank_1 = WrapMPRLS(multiplexerLine=multiplex[2])
-    if mprls_tank_1.cantConnect:
+    mprls_tank_1 = MPRLSWrappedSensor(multiplexer_line=multiplex[2])
+    if mprls_tank_1.cant_connect:
         mprint.p("COULD NOT CONNECT TO TANK 1 MPRLS!! Time: " + str(timeMS()) + " ms", output_log)
 
     # Tank 2 MPRLS
-    mprls_tank_2 = WrapMPRLS(multiplexerLine=multiplex[3])
-    if mprls_tank_2.cantConnect:
+    mprls_tank_2 = MPRLSWrappedSensor(multiplexer_line=multiplex[3])
+    if mprls_tank_2.cant_connect:
         mprint.p("COULD NOT CONNECT TO TANK 2 MPRLS!! Time: " + str(timeMS()) + " ms", output_log)
 
     # Tank 3 MPRLS
-    mprls_tank_3 = WrapMPRLS(multiplexerLine=multiplex[4])
-    if mprls_tank_3.cantConnect:
+    mprls_tank_3 = MPRLSWrappedSensor(multiplexer_line=multiplex[4])
+    if mprls_tank_3.cant_connect:
         mprint.p("COULD NOT CONNECT TO TANK 3 MPRLS!! Time: " + str(timeMS()) + " ms", output_log)
     
     mprint.p("MPRLS' connected. Time: " + str(timeMS()) + " ms", output_log)
