65b4b00f4fb169987d174f896aeb742d6bb29544
ALEEF02 <thesmarty2002@gmail.com>
2025-04-08 10:47:03 -0400

    SampleUpwards, sans Sample Collection c, complete

--- pi/processes/process_sample_upwards.py
+++ pi/processes/process_sample_upwards.py
@@ -9,6 +9,7 @@ from pi.processes.process_log_pressures import LogPressures
 from pi.collection import Collection
 from pi.tank import TankState
 from pi.valve import Valve
+from pi.MPRLS import PressureSensor
 
 class SampleUpwards(Process):
 
@@ -17,6 +18,10 @@ class SampleUpwards(Process):
         self.log_pressures: LogPressures
         self.main_valve: Valve
         self.dynamic_valve: Valve
+        self.manifold_pressure_sensor: PressureSensor
+
+        self.t_efficacy: float = 2 # seconds
+        self.p_choke: float
 
     def set_log_pressures(self, log_pressures_process: LogPressures):
         self.log_pressures = log_pressures_process
@@ -29,6 +34,9 @@ class SampleUpwards(Process):
 
     def set_dynamic_valve(self, dynamic_valve: Valve):
         self.dynamic_valve = dynamic_valve
+
+    def set_manifold_pressure_sensor(self, manifold_pressure_sensor: PressureSensor):
+        self.manifold_pressure_sensor = manifold_pressure_sensor
         
     def run(self) -> bool:
         print(type(Process.get_multiprint()))
@@ -61,6 +69,10 @@ class SampleUpwards(Process):
             Process.get_multiprint().pform("Dynamic Valve not set for Sample Upwards! Aborting Process.", Process.get_rtc().getTPlusMS(), Process.get_output_log())
             warn("Dynamic Valve not set for Sample Upwards!")
             return False
+        if not self.manifold_pressure_sensor:
+            Process.get_multiprint().pform("Manifold Pressure Sensor not set for Sample Upwards! Aborting Process.", Process.get_rtc().getTPlusMS(), Process.get_output_log())
+            warn("Manifold Pressure Sensor not set for Sample Upwards!")
+            return False
         return True
 
     def execute(self):
@@ -69,9 +81,33 @@ class SampleUpwards(Process):
         for c in self.collections:
 
             if c.tank.state == TankState.LAST_RESORT: # c.tank.status LAST_RESORT? Yes
-                Process.get_multiprint().pform(f"Tank {c.tank.valve.name} for Collection {c.num} is {c.tank.state}! Waiting to test for efficacy at T+{c.up_start_time - c.bleed_duration}s", 
+                Process.get_multiprint().pform(f"Tank {c.tank.valve.name} for Collection {c.num} is {c.tank.state}! Waiting to test for efficacy at T+{c.up_start_time - c.bleed_duration - self.t_efficacy}s", 
                                                Process.get_rtc().getTPlusMS(), Process.get_output_log())
-                # TODO: More stuff from right-side of the diagram
+                while Process.get_rtc().getTPlusMS() < (c.up_start_time - c.bleed_duration - self.t_efficacy): # Reached evaluation time (c.up_start_time - c.bleed_duration - t_efficacy)?
+                    self.log_pressures.run()
+
+                Process.get_multiprint().pform(f"Beginning Collection {c.num} efficacy test.", 
+                                               Process.get_rtc().getTPlusMS(), Process.get_output_log())
+                
+                self.main_valve.open()
+                self.dynamic_valve.open()
+                efficacy_start_time = Process.get_rtc().getTPlus()
+                while Process.get_rtc().getTPlus() < (efficacy_start_time + self.t_efficacy): #t_efficacy time passed?
+                    self.log_pressures.run()
+                
+                self.dynamic_valve.close()
+                self.main_valve.close()
+
+                post_efficacy_manifold_pressure = self.manifold_pressure_sensor.triple_pressure
+                post_efficacy_tank_pressure = c.tank.pressure_sensor.triple_pressure
+                if post_efficacy_manifold_pressure <= (post_efficacy_tank_pressure + self.p_choke): # Pmanifold > (Pc.tank+pchoke)? (using triple pressure) No
+                    Process.get_multiprint().pform(f"Manifold pressure ({post_efficacy_manifold_pressure} hPa) IS NOT much greater than tank pressure ({post_efficacy_tank_pressure} hPa)! Aborting Collection {c.num}!",
+                                                   Process.get_rtc().getTPlusMS(), Process.get_output_log())
+                    continue
+                else:
+                    Process.get_multiprint().pform(f"Manifold pressure ({post_efficacy_manifold_pressure} hPa) much greater than tank pressure ({post_efficacy_tank_pressure} hPa)!",
+                                                   Process.get_rtc().getTPlusMS(), Process.get_output_log())
+                    # Will if statement to begin collection from here.
                 
             elif c.tank.state != TankState.READY: # Is c.tank.status READY? No
                 Process.get_multiprint().pform(f"Tank {c.tank.valve.name} for Collection {c.num} is {c.tank.state}! Will not sample this Collection!", 
@@ -91,8 +127,15 @@ class SampleUpwards(Process):
 
             Process.get_multiprint().pform(f"Beginning Collection {c.num}", 
                                             Process.get_rtc().getTPlusMS(), Process.get_output_log())
+           
             # TODO: Sample lmao
 
-    
+            if c.tank.state == TankState.SAMPLED:
+                Process.get_multiprint().pform(f"Collection {c.num} succeeded (Tank {c.tank.valve.name} {c.tank.state})!", 
+                                            Process.get_rtc().getTPlusMS(), Process.get_output_log())
+            else:
+                Process.get_multiprint().pform(f"Collection {c.num} failed (Tank {c.tank.valve.name} {c.tank.state})!", 
+                                            Process.get_rtc().getTPlusMS(), Process.get_output_log())
+                
     def cleanup(self):
         Process.get_multiprint().pform("Finished Sample Upwards.", Process.get_rtc().getTPlusMS(), Process.get_output_log())
